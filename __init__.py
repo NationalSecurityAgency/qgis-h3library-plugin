cb004335d6af886f71252f3459c9f9ce329593be
C Hamilton <hamiltoncj@users.noreply.github.com>
2023-04-03 13:47:42 +0000

    Update H3 messages

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QAction, QMessageBox
 
 H3_MESSAGE = '''
     <p>
-      To create H3 density maps you will need to install the H3 Python Library (<a href="https://h3geo.org/">https://h3geo.org/</a>).<br><br>
+      To use H3 you will need to install the H3 Python Library (<a href="https://h3geo.org/">https://h3geo.org/</a>).<br><br>
       The H3 package can be installed by running the OSGeo4W shell as system administrator and running 'pip install h3' or whatever method you use to install python packages.
     </p>
     <p>
@@ -22,9 +22,9 @@ except Exception:
     import platform
     if platform.system() == 'Windows':
         site.addsitedir(os.path.abspath(os.path.dirname(__file__) + '/libs'))
-        H3_MESSAGE = '<p>H3 was successfully installed.</p><p>To uninstall, you must manually remove this plugin directory "{}" when QGIS is not running.</p><p>Using the QGIS plugin manager to remove it will not work.</p.'.format(os.path.abspath(os.path.dirname(__file__)))
+        H3_MESSAGE = 'H3 was successfully installed'
     else:
-        H3_MESSAGE = '<p>This plugin only works with Windows. You will need to install H3 as follows:</p>' + H3_MESSAGE
+        H3_MESSAGE = '<p>This plugin only works with Windows.</p>' + H3_MESSAGE
 
 def classFactory(iface):
     return H3Library(iface)
